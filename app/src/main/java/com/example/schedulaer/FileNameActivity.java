d55a489fc6004d4b661be8ceb890de9850e6f1d1
Raghava <moorthy46030@gmail.com>
2024-02-13 22:12:22 +0530

    Logic Changes

--- app/src/main/java/com/example/schedulaer/FileNameActivity.java
+++ app/src/main/java/com/example/schedulaer/FileNameActivity.java
@@ -1,6 +1,7 @@
 package com.example.schedulaer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -21,6 +22,10 @@ public class FileNameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.file_name);
 
+
+        toggleElements();
+        save = findViewById(R.id.saveBtn);
+        save.setOnClickListener(view -> validateDayHour());
         Button cancelButton = findViewById(R.id.button);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,22 +35,23 @@ public class FileNameActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        toggleElements();
-        validateDayHour();
     }
-    protected void toggleElements(){
+
+    protected void toggleElements() {
         className = findViewById(R.id.className);
         days = findViewById(R.id.days);
         hours = findViewById(R.id.hours);
         noOfDays = findViewById(R.id.no_of_days);
-        noOfHours  = findViewById(R.id.no_of_hours);
+        noOfHours = findViewById(R.id.no_of_hours);
         className.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String text = String.valueOf(className.getText());
-                if(text.length()>1){
+                if (text.length() > 1) {
                     days.setVisibility(View.VISIBLE);
                     hours.setVisibility(View.VISIBLE);
                     noOfDays.setVisibility(View.VISIBLE);
@@ -53,54 +59,32 @@ public class FileNameActivity extends AppCompatActivity {
                     save.setVisibility(View.VISIBLE);
                 }
             }
+
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
         });
     }
-    protected void validateDayHour(){
+
+    protected void validateDayHour() {
         days = findViewById(R.id.days);
         hours = findViewById(R.id.hours);
-        save = findViewById(R.id.saveBtn);
-        days.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int day = Integer.parseInt(String.valueOf(days.getText()));
-                if(day>6 || day==0){
-                    Toast.makeText(FileNameActivity.this, "Enter a Valid Number of Working Days", Toast.LENGTH_SHORT).show();
-                    days.setText("");
-                    return;
-                }
-            }
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-        hours.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int hour = Integer.parseInt(String.valueOf(hours.getText()));
-                if(hour>8 || hour<4){
-                    Toast.makeText(FileNameActivity.this, "Enter a Valid Number of Working Hours", Toast.LENGTH_SHORT).show();
-                    hours.setText("");
-                    return;
-                }
-            }
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-        findViewById(R.id.saveBtn).setOnClickListener(v -> {
-            if(String.valueOf(days.getText()).equals("") || String.valueOf(hours.getText()).equals("")) {
-                Toast.makeText(this, "Enter All Input Fields ", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(this, "Input Fields filled ", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
+        int day = Integer.parseInt(String.valueOf(days.getText()));
+        int hour = Integer.parseInt(String.valueOf(hours.getText()));
+        int flag = 0;
+        if (day == 0 || day > 5) {
+            Toast.makeText(FileNameActivity.this, "Enter a Valid Number of Working Days", Toast.LENGTH_SHORT).show();
+            days.setText("");
+            flag++;
+        }
+        if (hour > 8 || hour < 4) {
+            Toast.makeText(FileNameActivity.this, "Enter a Valid Number of Working Hours", Toast.LENGTH_SHORT).show();
+            hours.setText("");
+            flag++;
+        }
+        if (flag == 0) {
+            Toast.makeText(this, "Input Fields filled ", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
